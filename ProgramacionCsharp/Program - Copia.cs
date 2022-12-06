be307714552b55fb6da5bd6a29eab30a38613545
FedeDelca <fede.delcastillo@hotmail.com>
2022-12-06 19:52:43 -0300

    Agregar archivos de proyecto.

---
+++ ProgramacionCsharp/Program - Copia.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo!!");
+
+        int a = 10;
+        int b = 20;
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        int suma = a + b;
+
+        Console.WriteLine(suma);
+
+        string cadena = "Esto es una cadena de caracteres";
+
+        Console.WriteLine(cadena);
+        string cadena2 = "Esto es una cadena con los numeros a y b";
+        Console.WriteLine(cadena2);
+        string cadena3 = $"Esto es una cadena con los numeros {a} y {b}";
+
+        Console.WriteLine(cadena3);
+        string cadena4 = string.Format("Esto es una cadena con los numeros {0} y {1}", a, b);
+        Console.WriteLine(cadena4);
+    }
+}
---
+++ ProgramacionCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+float sueldo;
+string linea;
+
+Console.WriteLine("Ingrese el sueldo");
+linea = Console.ReadLine();
+
+
+sueldo = float.Parse(linea);
+
+if (sueldo > 3000)
+{
+    Console.WriteLine("Esta persona debe abonar impuestos");
+}
+else
+{
+    Console.WriteLine("Esta persona no debe pagar impuestos");
+}
+Console.ReadKey
